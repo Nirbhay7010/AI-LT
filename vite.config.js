e077282c344b109c4f5d0965a67598eaef739618
Nirbhay <soninirbhay74@gmail.com>
2025-08-23 00:02:40 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ai-based-lang-tralation/', // 👈 must match your GitHub repo name
 })
